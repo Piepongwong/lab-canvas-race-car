834208d31492ef9bd8351683186569ed7571da2e
Piepognwong <j.tonneyck@gmail.com>
2020-05-20 17:27:42 +0200

    review

--- js/index.js
+++ js/index.js
@@ -6,29 +6,28 @@ window.onload = () => {
 
 };
 
-class Game {
-  car = {}; // composition pattern
-  obstacles = []; // composition pattern
-  start(){
-    // intialize game and start rendering the game continiously
+function isCollide($dom1, $dom2) {
+  let sq1 = {
+      x: $dom1.offsetLeft,
+      y: $dom1.offsetTop,
+      width: $dom1.offsetWidth,
+      height: $dom1.offsetHeight
   }
-  
-  render(){
-    // the game manages the other game elements
-    this.car.render();
-    this.obstacles.forEach((obstacle)=> obstacle.render());
+  let sq2 = {
+      x: $dom2.offsetLeft,
+      y: $dom2.offsetTop,
+      width: $dom2.offsetWidth,
+      height: $dom2.offsetHeight
   }
-
-}
-
-class Car {
-  render(){
-    // car dom manipulation here
-  }
-}
-
-class Obstacle {
-  render(){
-    // obstacle dom manipution here
+  if (!(sq2.y + sq2.height <= sq1.y ||
+          sq2.y >= sq1.y + sq1.height ||
+          sq2.x + sq2.width <= sq1.x ||
+          sq2.x >= sq1.x + sq1.width
+      )) {
+      console.log("collision");
+      return true;
+  } else {
+      console.log("no collision");
+      return false;
   }
 }
